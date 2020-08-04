ec4d69e07cff6b01860291d49e67348d435a1ad8
sujay31 <sujay31rastogi@gmail.com>
2020-08-05 01:55:42 +0530

    Home page added

---
+++ editor.c
@@ -0,0 +1,18 @@
+/* header file contains input/output related functions. */
+#include <stdio.h>
+
+/* header file contains dynamic memory functions and exit function among others. */
+#include <stdlib.h>
+
+/* header file contains string related functions. */
+#include <string.h>
+
+/* header file contains character related functions. */
+#include <ctype.h>
+
+/* displays home page as landing page. */
+int main() 
+{
+    system("clear");
+    system("cat home.txt");
+}
