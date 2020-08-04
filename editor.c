04b1dab936369ad85008d00f07ae81489f3ab72c
sujay31 <sujay31rastogi@gmail.com>
2020-08-05 03:02:45 +0530

    Code for interface added

--- editor.c
+++ editor.c
@@ -15,4 +15,15 @@ int main()
 {
     system("clear");
     system("cat home.txt");
+    char choice;
+    printf("\033[1;33m ENTER W FOR WRITING, E FOR EDITING, S FOR RESAVING AN EXISTING FILE, F TO FIND A WORD, C TO COMPARE TWO FILES AND D TO DELETE A FILE. \033[0m \n");
+    scanf("%c", &choice);
+    if(choice=='W' || choice=='w')
+        system("cat interface.txt");
+    else
+    {
+        printf("\033[1;31m CHOICE UNAVAILABLE. PROGRAM HAS BEEN TERMINATED. \033[0m \n");
+        exit(1);
+    }
+    
 }
